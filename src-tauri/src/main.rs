66c9978bf6bc55977901d504d9b6170d5ae6f9e2
Deleteboys <Deleteboys.minecraft@web.de>
2024-04-25 14:26:55 +0200

    Fixed scroll bug, added method to enumerate all processes

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,9 +7,11 @@ use std::os::windows::ffi::OsStringExt;
 use std::path::Path;
 use std::process::exit;
 use std::{mem, ptr};
+use std::mem::size_of;
 use serde::Serialize;
 use serde_json::Value;
 use widestring::WideCString;
+use winapi::ctypes::{c_char, c_uchar};
 use winapi::shared::minwindef::{BOOL, DWORD, LPARAM, MAX_PATH};
 use winapi::shared::ntdef::NULL;
 use winapi::shared::windef::HWND;
@@ -20,6 +22,7 @@ use winapi::um::memoryapi::{VirtualAllocEx, WriteProcessMemory};
 use winapi::um::processthreadsapi::{CreateRemoteThread, OpenProcess};
 use winapi::um::winnt::{MEM_COMMIT, MEM_RESERVE, PAGE_READWRITE, PROCESS_ALL_ACCESS, PROCESS_VM_READ, PROCESS_VM_WRITE};
 use winapi::um::winuser::{EnumWindows, GetWindowTextLengthA, GetWindowTextW, GetWindowThreadProcessId, IsWindowVisible};
+use winapi::um::tlhelp32::{CreateToolhelp32Snapshot, Process32First, Process32Next, PROCESSENTRY32, TH32CS_SNAPMODULE, TH32CS_SNAPPROCESS};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -29,12 +32,60 @@ fn greet(name: &str) -> String {
 
 
 #[derive(Serialize)]
-struct processes {
+struct Process {
     name: String,
-    process_id: String
+    process_id: String,
 }
 
-static mut ALL_PROCESSES: Vec<processes> = vec![];
+static mut ALL_PROCESSES: Vec<Process> = vec![];
+
+fn enum_processes() {
+    unsafe {
+        let snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS | TH32CS_SNAPMODULE, 0);
+        if !snapshot.is_null() {
+            let mut proc_entry: PROCESSENTRY32 = PROCESSENTRY32 {
+                dwSize: 0,
+                cntUsage: 0,
+                th32ProcessID: 0,
+                th32DefaultHeapID: 0,
+                th32ModuleID: 0,
+                cntThreads: 0,
+                th32ParentProcessID: 0,
+                pcPriClassBase: 0,
+                dwFlags: 0,
+                szExeFile: [0; MAX_PATH],
+            };
+
+            proc_entry.dwSize = size_of::<PROCESSENTRY32>() as u32;
+            if Process32First(snapshot, &mut proc_entry as *mut PROCESSENTRY32) != 0 {
+                loop {
+                    let exe_file = proc_entry.szExeFile.clone();
+                    let mut exe_name = "Unkown".to_string();
+
+                    if !exe_file.is_empty() {
+                        if !exe_file.as_ptr().is_null() {
+                            let exe_file = mem::transmute::<Vec<i8>,Vec<u8>>(exe_file.to_vec());
+                            let length = String::from_utf8_unchecked(exe_file.clone()).find("\0").unwrap();
+                            let string = String::from_utf8_unchecked(exe_file.clone()[..length].to_owned());
+                            exe_name = string;
+                        }
+                    }
+                    println!("{:?} {:?}", proc_entry.th32ProcessID, exe_name.clone());
+                    let process_id = format!("{:#0X}", proc_entry.th32ProcessID);
+                    ALL_PROCESSES.push(Process {
+                        process_id: process_id.to_string(),
+                        name: exe_name.to_string(),
+                    });
+                    if Process32Next(snapshot, &mut proc_entry as *mut PROCESSENTRY32) == 0 {
+                        break;
+                    }
+                }
+            } else {
+                println!("Process32First failed: {:#x}", GetLastError());
+            }
+        }
+    }
+}
 
 extern "system" fn enum_windows_proc(hwnd: HWND, _: LPARAM) -> BOOL {
     unsafe {
@@ -54,9 +105,9 @@ extern "system" fn enum_windows_proc(hwnd: HWND, _: LPARAM) -> BOOL {
         //format u32 to hex
         let process_id = format!("{:#0X}", buf);
 
-        ALL_PROCESSES.push(processes{
+        ALL_PROCESSES.push(Process {
             process_id: process_id.to_string(),
-            name: window_title.to_str().unwrap().to_string()
+            name: window_title.to_str().unwrap().to_string(),
         });
         return 1;
     }
@@ -77,6 +128,16 @@ fn get_all_windows() -> String {
     }
 }
 
+#[tauri::command]
+fn get_all_processes() -> String {
+    unsafe {
+        ALL_PROCESSES = vec![];
+        enum_processes();
+        let output = convert_all_processes_to_json().expect("");
+        output
+    }
+}
+
 fn convert_hex_to_dword(input: &str) -> DWORD {
     let process_id = input;
     let remove_prefix = process_id.trim_start_matches("0x");
@@ -91,20 +152,20 @@ fn convert_hex_to_dword(input: &str) -> DWORD {
 }
 
 #[tauri::command]
-fn inject_dll(process_id: &str, dll_path: &str) -> String{
+fn inject_dll(process_id: &str, dll_path: &str) -> String {
     let dll_path = dll_path.trim();
     let dll_path = Path::new(dll_path);
     let proc_id = convert_hex_to_dword(process_id.trim());
-    
+
     inject_into_process(proc_id, dll_path)
 }
 
 
-fn inject_into_process(proc_id: DWORD, dll: &Path) -> String{
+fn inject_into_process(proc_id: DWORD, dll: &Path) -> String {
     unsafe {
         let h_process = OpenProcess(PROCESS_ALL_ACCESS | PROCESS_VM_WRITE | PROCESS_VM_READ, 0, proc_id);
         if h_process == NULL {
-            return(format!("Program could not be found the process id was {:#0X}", proc_id));
+            return (format!("Program could not be found the process id was {:#0X}", proc_id));
             // exit(1)
         }
         let dll_path = dll;
@@ -118,7 +179,7 @@ fn inject_into_process(proc_id: DWORD, dll: &Path) -> String{
 
         let mut allocate_memory = VirtualAllocEx(h_process, ptr::null_mut(), MAX_PATH, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
         if allocate_memory == NULL {
-            return("Hooking the memory didn't work").to_string();
+            return ("Hooking the memory didn't work").to_string();
             // exit(1)
         }
 
@@ -129,7 +190,7 @@ fn inject_into_process(proc_id: DWORD, dll: &Path) -> String{
                               ptr::null_mut()) == 0 {
             let error = GetLastError();
             println!("{:?}", error);
-            return("Writing Memory went wrong").to_string();
+            return ("Writing Memory went wrong").to_string();
             // exit(1)
         }
         let kernel32 = CString::new("kernel32.dll").expect("CString::new failed");
@@ -137,25 +198,25 @@ fn inject_into_process(proc_id: DWORD, dll: &Path) -> String{
 
         let h_kernel32 = GetModuleHandleA(kernel32.as_ptr());
         if h_kernel32.is_null() {
-            return("Failed to get the handle of kernel32.dll.").to_string();
+            return ("Failed to get the handle of kernel32.dll.").to_string();
             // exit(1);
         }
         let h_loadlibraryw =
             GetProcAddress(h_kernel32, loadlibraryw.as_ptr());
         if h_loadlibraryw.is_null() {
-            return("Failed to get the address of LoadLibraryW.").to_string();
+            return ("Failed to get the address of LoadLibraryW.").to_string();
             // exit(1)
         }
         let hthread = CreateRemoteThread(h_process, ptr::null_mut(), 0, Some(mem::transmute(h_loadlibraryw)), allocate_memory, 0, ptr::null_mut());
 
         CloseHandle(hthread);
-        return("Injected successfully").to_string()
+        return ("Injected successfully").to_string();
     }
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![get_all_windows, inject_dll])
+        .invoke_handler(tauri::generate_handler![get_all_windows, inject_dll, get_all_processes])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -9,29 +9,39 @@
     <v-responsive class="d-flex justify-center fill-height">
       <v-card-title class="text-center text-h3" style="margin-bottom: 20px">Hydrus</v-card-title>
       <v-card class="rounded-xl" style="height: 84%" elevation="0">
-        <v-card-text class="text-center text-h6">Select a window:</v-card-text>
+        <v-card-actions>
+          <v-btn-toggle style="margin: 0 auto 0 auto;" variant="outlined" color="primary" divided
+                        v-model="applicationType" mandatory>
+            <v-btn>Windows</v-btn>
+            <v-btn>Processes</v-btn>
+          </v-btn-toggle>
+        </v-card-actions>
+        <v-card-text class="text-center text-h6">Select a window or process:</v-card-text>
         <v-card-actions>
           <v-autocomplete class="rounded-xl" rounded variant="solo-filled" label="Process" v-model="process"
-                    :items="JSON.parse(greetMsg)" item-value="process_id" item-title="name"
-                    :item-props="true"></v-autocomplete>
+                          :items="JSON.parse(greetMsg)" item-value="process_id" item-title="name"
+                          :item-props="true"></v-autocomplete>
         </v-card-actions>
         <v-card-text class="text-center text-h6">Select a DLL</v-card-text>
         <v-card-actions>
           <!--          <v-file-input class="rounded-xl" rounded variant="solo-filled" label="DLL" prepend-icon="" v-model="dll_path"></v-file-input>-->
-          <v-text-field label="DLL" rounded variant="solo-filled" readonly @click="readPath" v-model="dll_path"></v-text-field>
+          <v-text-field label="DLL" rounded variant="solo-filled" readonly @click="readPath"
+                        v-model="dll_path"></v-text-field>
         </v-card-actions>
         <v-card-actions>
-          <v-btn style="margin: 0 auto 0 auto; margin-top: 20px" variant="elevated" color="primary" class="w-50" @click="inject_dll">
+          <v-btn style="margin: 0 auto 0 auto; margin-top: 0px" variant="elevated" color="primary" class="w-50"
+                 @click="inject_dll">
             Inject
           </v-btn>
         </v-card-actions>
         <v-card-actions>
-          <v-btn style="margin: 0 auto 0 auto;" variant="elevated" color="primary" class="w-50" @click="update_process_list">
+          <v-btn style="margin: 0 auto 0 auto;" variant="elevated" color="primary" class="w-50"
+                 @click="update_process_list">
             Refresh
           </v-btn>
         </v-card-actions>
-<!--        <v-card-text>{{ process }}</v-card-text>-->
-<!--        <v-card-text>{{ dll_path }}</v-card-text>-->
+        <!--        <v-card-text>{{ process }}</v-card-text>-->
+        <!--        <v-card-text>{{ dll_path }}</v-card-text>-->
       </v-card>
     </v-responsive>
   </v-container>
@@ -50,6 +60,7 @@ export default {
       dll_path: "",
       greetMsg: "[]",
       inject_output: "",
+      applicationType: 0
     }
   },
   methods: {
@@ -64,7 +75,11 @@ export default {
       this.dll_path = path
     },
     async update_process_list() {
-      this.greetMsg = await invoke("get_all_windows");
+      if (this.applicationType === 0) {
+        this.greetMsg = await invoke("get_all_windows");
+      } else {
+        this.greetMsg = await invoke("get_all_processes");
+      }
     },
     async inject_dll() {
       this.inject_output = await invoke("inject_dll", {processId: this.process, dllPath: this.dll_path});
